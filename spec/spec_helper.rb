155e7b271e840bc0891ed54c9d9415681321b5e2
Ladislav Slezák <lslezak@suse.cz>
2014-12-04 10:56:17 +0100

    always run test code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,22 @@
 # encoding: utf-8
 
-if ENV["COVERAGE"]
-  require "simplecov"
+require "simplecov"
 
-  # use coveralls for on-line code coverage reporting at Travis CI
-  if ENV["TRAVIS"]
-    require "coveralls"
+# use coveralls for on-line code coverage reporting at Travis CI
+if ENV["TRAVIS"]
+  require "coveralls"
 
-    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-      SimpleCov::Formatter::HTMLFormatter,
-      Coveralls::SimpleCov::Formatter
-    ]
-  end
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+end
 
-  SimpleCov.minimum_coverage 95
+SimpleCov.minimum_coverage 95
 
-  SimpleCov.start
+SimpleCov.start do
+  add_filter "/vendor/"
+  add_filter "/spec/"
 end
 
 # allow only the new "expect" RSpec syntax
